f95bbd85d24b2e8d77469c57fe078e727de1ac8c
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-03-09 13:17:57 -0800

    Adjusted styling on SignIn and SignUp pages to have clerk auth centered in page, added UserButton for testing sign out on dashboard page

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full min-h-screen grid place-items-center">
+      <SignUp />
+    </div>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { UserButton } from "@clerk/clerk-react";
+
 export default function Dashboard({ params }: { params: { userId: string } }) {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <UserButton />
+      <div>Dashboard</div>
+    </div>
+  );
 }
