bd10f4264c94b20ee04bcd0f7f4cc72ec80afac6
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-03-12 12:51:30 -0700

    Moved HomeNavbar from page to layout on home due to main component stylish conflicts on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import HomeNavbar from "@/components/Navbars/HomeNavbar";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HomeNavbar />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         Home Page
       </div>
